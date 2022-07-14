eea71975a32c3256ae4707614082c370c7778e8f
Annie Bauer <overheads165@gmail.com>
2022-07-14 15:16:30 +0300

    eslint

--- src/index.js
+++ src/index.js
@@ -1,20 +1,6 @@
-const pers = {
-  name: "Маг",
-  health: 90,
-};
+const indication = (pers) => (pers.health > 50 ? 'healthy' : pers.health < 15 ? 'critical' : 'wounded');
 
-const indication = (pers) =>
-  pers.health > 50 ? "healthy" : pers.health < 15 ? "critical" : "wounded";
-
-const persInfo = [
-  { name: "мечник", health: 10 },
-  { name: "маг", health: 100 },
-  { name: "лучник", health: 80 },
-];
-
-const sortPersInfo = (array) =>
-  array.sort((a, b) =>
-    a.health > b.health ? -1 : a.health < b.health ? +1 : 0
-  );
+const sortingFunction = (a, b) => a.health > b.health ? -1 : a.health < b.health ? +1 : 0;
+const sortPersInfo = (array) => array.sort(sortingFunction);
 
 export { sortPersInfo, indication };
--- src/index.test.js
+++ src/index.test.js
@@ -1,45 +1,45 @@
-import { indication, sortPersInfo } from "./index";
+import { indication, sortPersInfo } from './index';
 
-test("больше 50 - healthy", () => {
+test('больше 50 - healthy', () => {
   const input = {
-    name: "Маг",
+    name: 'Маг',
     health: 90,
   };
-  const expected = "healthy";
+  const expected = 'healthy';
   const received = indication(input);
   expect(received).toBe(expected);
 });
 
-test("меньше 15 - critical", () => {
+test('меньше 15 - critical', () => {
   const input = {
-    name: "Маг",
+    name: 'Маг',
     health: 10,
   };
-  const expected = "critical";
+  const expected = 'critical';
   const received = indication(input);
   expect(received).toBe(expected);
 });
 
-test("от 15 до 50 - wounded", () => {
+test('от 15 до 50 - wounded', () => {
   const input = {
-    name: "Маг",
+    name: 'Маг',
     health: 38,
   };
-  const expected = "wounded";
+  const expected = 'wounded';
   const received = indication(input);
   expect(received).toBe(expected);
 });
 
-test("сортировка массива по убыванию", () => {
+test('сортировка массива по убыванию', () => {
   const input = [
-    { name: "мечник", health: 10 },
-    { name: "маг", health: 100 },
-    { name: "лучник", health: 80 },
+    { name: 'мечник', health: 10 },
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
   ];
   const expected = [
-    { name: "маг", health: 100 },
-    { name: "лучник", health: 80 },
-    { name: "мечник", health: 10 },
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+    { name: 'мечник', health: 10 },
   ];
 
   const received = sortPersInfo(input);
